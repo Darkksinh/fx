0ca4c478d15a30362c47bce0315569a6429952f7
Darkksinh <78621169+Darkksinh@users.noreply.github.com>
2021-03-01 01:10:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
+namespace Google\Cloud\Samples\Vision;
+
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+
+// $path = 'gs://cloud-samples-data/vision/ocr/sign.jpg'
+
+function detect_text_gcs($path)
 {
-  "requests": [
-    {
-      "image": {
-        "source": {
-          "imageUri": "www.xvideos.com"
+    $imageAnnotator = new ImageAnnotatorClient();
+
+    # annotate the image
+    $response = $imageAnnotator->textDetection($path);
+    $texts = $response->getTextAnnotations();
+
+    printf('%d texts found:' . PHP_EOL, count($texts));
+    foreach ($texts as $text) {
+        print($text->getDescription() . PHP_EOL);
+
+        # get bounds
+        $vertices = $text->getBoundingPoly()->getVertices();
+        $bounds = [];
+        foreach ($vertices as $vertex) {
+            $bounds[] = sprintf('(%d,%d)', $vertex->getX(), $vertex->getY());
         }
-       },
-       "features": [
-         {
-           "type": "ruiva"
-         }
-       ]
+        print('Bounds: ' . join(', ', $bounds) . PHP_EOL);
     }
-  ]
+
+    if ($error = $response->getError()) {
+        print('API Error: ' . $error->getMessage() . PHP_EOL);
+    }
+
+    $imageAnnotator->close();
 }
